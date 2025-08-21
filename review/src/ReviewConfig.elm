ada5d1698f6a6e73ec1327c371266dc334087d70
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-08-21 11:52:38 +0200

    Add my standard config

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -12,14 +12,19 @@ when inside the directory containing this file.
 -}
 
 import Docs.ReviewAtDocs
+import NoAlways
+import NoBrokenParserFunctions
+import NoCatchAllForSpecificRemainingPatterns
 import NoConfusingPrefixOperator
 import NoDebug.Log
 import NoDebug.TodoOrToString
 import NoExposingEverything
 import NoImportingEverything
+import NoInconsistentAliases
 import NoMissingTypeAnnotation
 import NoMissingTypeAnnotationInLetIn
 import NoMissingTypeExpose
+import NoModuleOnExposedNames
 import NoPrematureLetComputation
 import NoSimpleLetBody
 import NoUnused.CustomTypeConstructorArgs
@@ -30,29 +35,55 @@ import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
 import Review.Rule as Rule exposing (Rule)
+import ReviewPipelineStyles
+import ReviewPipelineStyles.Fixes
 import Simplify
+import Validate.Regexes
 
 
 config : List Rule
 config =
     [ Docs.ReviewAtDocs.rule
+    , NoAlways.rule
+    , NoBrokenParserFunctions.rule
+    , NoCatchAllForSpecificRemainingPatterns.rule
     , NoConfusingPrefixOperator.rule
     , NoDebug.Log.rule
-    , NoDebug.TodoOrToString.rule
-        |> Rule.ignoreErrorsForDirectories [ "tests/" ]
+    , NoDebug.TodoOrToString.rule |> Rule.ignoreErrorsForDirectories [ "tests/" ]
     , NoExposingEverything.rule
     , NoImportingEverything.rule []
+    , NoInconsistentAliases.config
+        [ ( "Nix.Syntax.Node", "Node" )
+        , ( "Parser.Advanced", "Parser" )
+        ]
+        |> NoInconsistentAliases.noMissingAliases
+        |> NoInconsistentAliases.rule
     , NoMissingTypeAnnotation.rule
     , NoMissingTypeAnnotationInLetIn.rule
     , NoMissingTypeExpose.rule
-    , NoSimpleLetBody.rule
+    , NoModuleOnExposedNames.rule
     , NoPrematureLetComputation.rule
-    , NoUnused.CustomTypeConstructors.rule []
+    , NoSimpleLetBody.rule
     , NoUnused.CustomTypeConstructorArgs.rule
+    , NoUnused.CustomTypeConstructors.rule []
     , NoUnused.Dependencies.rule
     , NoUnused.Exports.rule
     , NoUnused.Parameters.rule
     , NoUnused.Patterns.rule
     , NoUnused.Variables.rule
+    , ReviewPipelineStyles.rule pipelineConfig |> Rule.ignoreErrorsForDirectories [ "tests/" ]
     , Simplify.rule Simplify.defaults
+    , Validate.Regexes.rule
+    ]
+
+
+pipelineConfig : List (ReviewPipelineStyles.PipelineRule ())
+pipelineConfig =
+    [ ReviewPipelineStyles.forbid ReviewPipelineStyles.leftPizzaPipelines
+        |> ReviewPipelineStyles.andTryToFixThemBy ReviewPipelineStyles.Fixes.convertingToParentheticalApplication
+        |> ReviewPipelineStyles.andCallThem "forbidden <| pipeline"
+    , ReviewPipelineStyles.forbid ReviewPipelineStyles.leftCompositionPipelines
+        |> ReviewPipelineStyles.andCallThem "forbidden << composition"
+    , ReviewPipelineStyles.forbid ReviewPipelineStyles.rightCompositionPipelines
+        |> ReviewPipelineStyles.andCallThem "forbidden >> composition"
     ]
